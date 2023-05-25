eb7a64486e263b0fe8ae16aa7b781e8cb24a7f8f
Stevenrej <steveo732@gmail.com>
2023-05-25 17:15:58 -0500

    content and styling added

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,8 +1,21 @@
 
 function Experience() {
   return (
-    <section id="experience">
+    <section id="experience" className="experience-section">
       <h2>Experience</h2>
+      <div className="experience">
+        <h3>Pets-Chicago, Freelance Marketing Consultant</h3>
+        <p>Iterated and refined existing and future brand materials for this 20+-year-old, family-owned Chicago business catering to busy professionals in urban neighborhoods. Communicate clearly and effectively with clients and coworkers to understand organizational and client needs.</p>
+
+        <h3>Ana Cahill Creative Agency, Associate</h3>
+        <p>Created social media content for a number of clients for this Nashville-based boutique that focuses on small businesses and entrepreneurs. Created Instagram Reels, TikTok and LinkedIn content; used Lately, Hootsuite, Buffer. Maintain and update online shops for several clients; edit audio and video for podcasts, social media.</p>
+
+        <h3>Why of AI, Associate (Practicum Course at Columbia College Chicago)</h3>
+        <p>Co-lead team of five representing Chicago tech startup Why of AI: A service-oriented company that provides training and online courses centered around AI literacy, development, and strategy. Iterated and refined existing and future brand guidelines, developed a SOP for shared media and online engagement, created one-pagers, helped develop concepts for a company explainer video, PR strategies, and more.</p>
+
+        <h3>Lost Larson, Associate (360 Experiential Campaigns at Columbia College Chicago)</h3>
+        <p>Worked within a team; delivered an activation deck for local bakery, Lost Larson. Deck contained research on the client, the corresponding market, challenges & opportunities, creative concepts, & budgeting. Deck incorporated both primary and secondary research; research included interviews with current Lost Larson patrons, social media analysis, compiled data from platforms like Mintel and Statista, and incorporated strategic concepts from relevant use cases.</p>
+      </div>
     </section>
   );
 }
