6a0ad3e22fec433c7324a9ba655cf4fe8735dca8
Stevenrej <steveo732@gmail.com>
2023-05-25 17:16:38 -0500

    content and styling added

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,8 +1,12 @@
 
+import profilePicture from '../assets/robertfari.jpeg';
+
 function Title() {
   return (
-    <section id="title">
-      <h2>Title</h2>
+    <section id="title" className="title-section">
+      <img src={profilePicture} alt="Placeholder" className="profile-image"/>
+      <h1>Robert Fornari</h1>
+      <p>I’m an early Marketing/Advertising professional with both client-side and agency experiences.</p>
     </section>
   );
 }
